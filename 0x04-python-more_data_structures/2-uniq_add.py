3a60ce028977f3202c1d6d09396f02da32d87f45
Julius Kamara <juliuskamara79@gmail.com>
2022-12-09 11:41:57 -0800

    This project adds all unique item in a list

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# _ _ _ _ _ THIS PROJECT ADDS ALL UNIQUE ITEM IN A LIST _ _ _ _ _ 
+# _ _ _ _ _ THIS PROJECT ADDS ALL UNIQUE ITEM IN A LIST _ _ _ _ _
 def uniq_add(my_list=[]):
     sum = 0
     for i in set(my_list):
