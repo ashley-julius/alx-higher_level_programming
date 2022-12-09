793d9717db0d1e476803e6a41276be6955db418c
Julius Kamara <juliuskamara79@gmail.com>
2022-12-09 11:04:23 -0800

    This project provides more insight on nested

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-# _ _ _ _ _ THIS PROJECT SQUARED THE ITEMS IN A NESTED LIST _ _ _ _ _ _ 
+# _ _ _ _ _ THIS PROJECT SQUARED THE ITEMS IN A NESTED LIST _ _ _ _ _ _
 def square_matrix_simple(matrix=[]):
     #  _ _ _ _ _ _ COPY OF THE LIST _ _ _ _ _ _ _
     square = []
-    # _ _ FOR LOOP _ _ 
+    # _ _ FOR LOOP _ _
     for row in matrix:
         square.append([num ** 2 for num in row])
     return square
-
