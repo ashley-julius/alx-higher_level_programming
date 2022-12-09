e5c76b122937975c886d0fa3b6bb9d131e3a87a2
Julius Kamara <juliuskamara79@gmail.com>
2022-12-09 11:00:04 -0800

    This project provides more insight on nested

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,10 +4,7 @@ def square_matrix_simple(matrix=[]):
     #  _ _ _ _ _ _ COPY OF THE LIST _ _ _ _ _ _ _
     square = []
     # _ _ FOR LOOP _ _ 
-    for i in range(3):
-        item = []
-        for j in range(3):
-            item.append(matrix[i][j] ** 2)
-        square.append(item)
+    for row in matrix:
+        square.append([num ** 2 for num in row])
     return square
 
