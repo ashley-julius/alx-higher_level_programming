4259dcb5b20537e7177830bc8ed6fe4879f5b293
Julius Kamara <juliuskamara79@gmail.com>
2022-12-10 04:18:04 -0800

    This project prints the keys in a sorted order

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# _ _ THIS PROJECT PRINTS THE KEYS IN A DICTIONARY IN SORTED ORDER _ _ 
+def print_sorted_dictionary(a_dictionary):
+    for key in sorted(a_dictionary.keys()):
+        print("{}: {}".format(key, a_dictionary[key]))
