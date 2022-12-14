0c0a8504e39a7122d5df1f606984c326bc647cf6
Julius Kamara <juliuskamara79@gmail.com>
2022-12-14 00:35:16 -0800

    Made some modification

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
     # _ _ DEFINING TWO VARIABLES _ _
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b))
