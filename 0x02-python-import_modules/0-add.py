fa4190579bac837ec4bac89eeb66988ac8383bbe
Julius Kamara <juliuskamara79@gmail.com>
2022-12-14 00:28:29 -0800

    Made some modification

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 # _ THIS PROJECT BUILDS ON MODULE IN PYTHON
-from add_0 import add
-# _ _ DEFINING TWO VARIABLES _ _
-a = 1
-b = 2
-sum = add(a, b)
-print("%2d + %2d = %2d" % (a, b, sum))
+if __name__ == '__main__':
+    from add_0 import add
+    # _ _ DEFINING TWO VARIABLES _ _
+    a = 1
+    b = 2
+    sum = add(a, b)
+    print("%2d + %2d = %2d" % (a, b, sum))
