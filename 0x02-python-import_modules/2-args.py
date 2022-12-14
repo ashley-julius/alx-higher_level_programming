9346e20e5657929f615a9101c90c0efe4cc2d045
Julius Kamara <juliuskamara79@gmail.com>
2022-12-14 07:54:30 -0800

    Awesome project on Commandline arguments in python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,14 +3,14 @@
 if __name__ == "__main__":
     import sys
 
-    # _ _ _ _TOTAL ARGUMENTS
+    # _ _ _ _TOTAL ARGUMENTS = total - name of program(1)
     n = len(sys.argv) - 1
     # _ _ _ CHECK IF N IS 0, 1 OR > 1
     if n == 0:
         print("0 arguments.")
     elif n == 1:
         print("1 argument:")
-    elif n > 1:
-        print("{:d} arguments.".format(n))
-    for i in range(1, len(sys.argv)):
-        print("{:d}: {}".format(i, sys.argv[i]))
+    else:
+        print("{:d} arguments:".format(n))
+    for i in range(n):
+        print("{:d}: {}".format((i + 1), sys.argv[i + 1]))
