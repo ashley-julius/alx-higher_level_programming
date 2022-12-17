b62fa350dbc6a9820218a2342743d7c94f731ff3
Julius Kamara <juliuskamara79@gmail.com>
2022-12-17 09:06:46 -0800

    New file

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,29 +2,37 @@
 # _ _ _ _ BUILDING MY OWN CALCULATOR _ _ _ _
 if __name__ == "__main__":
     import sys
-    from calculator_1 import add, sub, a, b, mul, div
+    from calculator_1 import add, sub, mul, div
     operators = ['+', '-', '*', '/']
+    a = 0
+    b = 0
     # _ _ _ CHECKING IF THE ARGUMENTS SUM UP TO THREE
     if len(sys.argv) < 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        return 1
+        print("1")
+        exit(1)
 
-    # _ _ CHECKING IF THE ANY OF THE OPERATORS IS IN SYS.ARGV
-    argu_ment = sys.argv[:]
-    if argu_ment[2] not in operators:
-        print("Unknown operator. Available operators: +, -, * and /")
-        return 1
     # _ _ COLLECTING THE TWO VALUES AND THE OPERATOR TO USE _ _
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-
-    # _ _ _ RETURN WHICH FUNCTION BASED ON THE OPERATOR FOUND  _ _ _ _
-    match argu_ment[2]:
-        case "+":
-            print("{:d} + {:d} = {:d}".format(a, b, (add(a, b))))
-        case "-":
-            print("{:d} - {:d} = {:d}".format(a, b, (sub(a, b))))
-        case "*":
-            print("{:d} * {:d} = {:d}".format(a, b, (mul(a, b))))
-        case _:
-            print("{:d} / {:d} = {:d}".format(a, b, (div(a, b))))
+    i = 1
+    for idx, num in enumerate(sys.argv[1:]):
+        if idx == 1:
+            a = int(num)
+            continue
+        elif idx == 3 and int(num) in range(0, 1000):
+            b = int(num)
+            continue
+        # _ _ RETURN A FUNCTION BASED ON THE OPERATOR _ _
+        elif idx == 2 and num in operators:
+            if num == '+':
+                print("{:d} + {:d} = {:d}".format(a, b, (add(a, b))))
+            elif num == '-':
+                print("{:d} - {:d} = {:d}".format(a, b, (sub(a, b))))
+            elif num == '*':
+                print("{:d} * {:d} = {:d}".format(a, b, (mul(a, b))))
+            elif num == '/':
+                print("{:d} / {:d} = {:d}".format(a, b, (div(a, b))))
+            elif i == 2 and num not in ['+', '-', '*', '/']:
+                print("Unknown operator. Available operators: +, -, * and /")
+                print("1")
+                exit(1)
+        i += 1
