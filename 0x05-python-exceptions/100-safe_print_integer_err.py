3a38311ba3fb39b94ee9ed63d2579cdf17b5348d
Julius Kamara <juliuskamara79@gmail.com>
2022-12-19 18:49:57 -0800

    This program shows how to handle exception whilst redirecting a message to stderr

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+# THIS PROJECT SHOW HOW TO PRINTING IMMEDIATE ERROR MESSAGES TO STDERR
+def safe_print_integer_err(value):
+    """Display message to stderr if an exception occurs"""
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError:
+        print("Exception: Unknown format code 'd' for object of type\
+ 'str'", file=sys.stderr)
+        return False
