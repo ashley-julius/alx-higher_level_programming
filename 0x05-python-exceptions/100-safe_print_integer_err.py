abdbfa1acb47d467e545977292edfec2a9af87c3
Julius Kamara <juliuskamara79@gmail.com>
2022-12-19 18:54:31 -0800

    This program shows how to handle exception whilst redirecting a message to stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import sys
 # THIS PROJECT SHOW HOW TO PRINTING IMMEDIATE ERROR MESSAGES TO STDERR
+
+
 def safe_print_integer_err(value):
     """Display message to stderr if an exception occurs"""
     try:
