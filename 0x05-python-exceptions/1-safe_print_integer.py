902490d2f34cd96d6d596dc58dcacf2af2e20e14
Julius Kamara <juliuskamara79@gmail.com>
2022-12-19 16:15:37 -0800

    This is the second hands on project on exceptions in python

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,10 +4,10 @@ def safe_print_integer(value):
     """Checks for int or string"""
     try:
         if value > 0:
-            print(value)
+            print("{:d}".format(value))
             return True
         elif value <= 0:
-            print(value)
+            print("{:d}".format(value))
             return True
     except TypeError:
         return False
