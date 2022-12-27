f122eaaf1019e4fdd9ebc60c13c413cf4f03c958
Julius Kamara <juliuskamara79@gmail.com>
2022-12-26 20:12:50 -0800

    This program provides a hands on experience with getter and setter

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -65,8 +65,8 @@ class Square():
                 value: instance attribute
         """
         if type(value) is tuple and len(value) is 2 and \
-            if type(value[0]) is int and type(value[1]) is int and \
-                if value[0] >= 0 and value[1] >= 0:
+            type(value[0]) is int and type(value[1]) is int and \
+                value[0] >= 0 and value[1] >= 0:
             self.__position = value
         else:
             raise TypeError("position must be a tuple of 2 positive\
