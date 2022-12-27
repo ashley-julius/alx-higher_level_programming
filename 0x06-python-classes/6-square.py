e491f284cad374676b311a27004169065fb1629b
Julius Kamara <juliuskamara79@gmail.com>
2022-12-26 17:59:17 -0800

    This program provides a hands on experience with getter and setter

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+"""This module defines a Square"""
+
+
+class Square():
+    """
+        Square: defines a square
+        Attributes:
+                size: To know the length of sides, area and perimeter
+
+        Method:
+                __init__: Initializing the Square attribute
+
+                area: Calculates the area of square
+     """
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializing the instance attribute
+
+        Args:
+            Size attributes
+            position attributes
+        """
+        self.__size = size
+        self.__position = position
+    # Using property decorator
+    # a getter function
+
+    @property
+    def size(self):
+
+        """
+            size: to return the size attribute
+        """
+        return self.__size
+    # setter function
+
+    @size.setter
+    def size(self, value):
+        """
+            size: To set the size attribute
+
+            Arguments:
+                value: an integer or string
+        """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    #getters function
+    def position(self):
+        """
+            position: retrieves the position
+        """
+        return self.__position
+    @position.setter
+    def position(self, value):
+        """
+            position: sets the position attributes
+            Argument:
+                value: instance attribute
+        """
+        if value[0] < 0 and value[1] < 0:
+            raise TypeError("position must be a tuple of 2\
+ positive integers")
+
+    def area(self):
+        """Calculates the area of square"""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """
+            my_print: prints # or blank line to stdout
+        """
+        if self.__size == 0:
+            print()
+        for i in range(0, self.__size):
+            if self.__position[1] <= 0:
+                print(" " * self.__position[0], end='')
+            elif self.__position[1] > 0:
+                print(" " * self.__position[0], end='')
+            print("#" * self.__size)
