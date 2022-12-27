ca3d1b29d09676bb582c38017c10b39764aca6b2
Julius Kamara <juliuskamara79@gmail.com>
2022-12-26 19:18:50 -0800

    This program provides a hands on experience with getter and setter

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -73,6 +73,7 @@ class Square():
         if type(value) != tuple or len(value) != 2:
             raise TypeError("position must be a tuple of 2\
  positive integers")
+        self.__position = value
 
     def area(self):
         """Calculates the area of square"""
