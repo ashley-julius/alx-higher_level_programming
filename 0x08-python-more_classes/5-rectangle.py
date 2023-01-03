5ff6fae44855f11d34b8cbadb1278f82d688022c
Julius Kamara <juliuskamara79@gmail.com>
2023-01-02 17:37:08 -0800

    This program shows us how to destroy an object and how to recreate new instance by using eval()

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -79,7 +79,8 @@ class Rectangle:
         items = ""
         items += "Rectangle(" + str(self.__width) + ', ' \
             + str(self.__height) + ')'
-        return eval(repr(items[:-1]))
+        new = eval(repr(items[:-1]))
+        return new
 
     def __del__(self):
         """This method destroys a class instance"""
