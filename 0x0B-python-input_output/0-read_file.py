c269032faa807f4728d60075aed0523189ca4ad9
Julius Kamara <juliuskamara79@gmail.com>
2023-01-23 19:14:04 -0800

    This project shows us how to read the entire content from a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ This module provides a hands-on experience on opening a file"""
 
+
 def read_file(filename=""):
     """ This function reads the entire content of a file"""
     with open(filename, 'r', encoding='utf-8') as fp:
