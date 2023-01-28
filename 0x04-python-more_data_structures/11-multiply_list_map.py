269e8c6c39e038108119a7f122fbc32eeb040d22
Julius Kamara <juliuskamara79@gmail.com>
2023-01-28 10:55:52 -0800

    This project multiplies a list by a number without using loop

---
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This module returns a list with values multiplied without using
+    loops
+"""
+
+
+def multiply_list_map(my_list=[], number=0):
+    """ This function multiplies a list without using loop """
+    return list(map(lambda x : x * number, my_list[:]))
