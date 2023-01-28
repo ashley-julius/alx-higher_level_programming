74760cc037678e8c950e4f32bf9d66a6318ce584
Julius Kamara <juliuskamara79@gmail.com>
2023-01-28 09:53:20 -0800

    This project multiplies all values in a dictionary by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -4,7 +4,7 @@
 
 def multiply_by_2(a_dictionary):
     """ This function multiplies the values by 2"""
-    b_dictionary = a_dictionary
+    b_dictionary = dict(list(a_dictionary.items())[:])
     for key, value in b_dictionary.items():
         b_dictionary[key] = value * 2
     return b_dictionary
