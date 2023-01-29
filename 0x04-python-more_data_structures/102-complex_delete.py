67b87a52f8655c3c69baad9e791205b1bf8bc48b
Julius Kamara <juliuskamara79@gmail.com>
2023-01-28 17:32:40 -0800

    This project teaches us how to delete a key that its value matches what we are looking for

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -4,8 +4,18 @@
 
 def complex_delete(a_dictionary, value):
     """ This function deletes keys with specific values """
-    if value in a_dictionary.items():
-        del a_dictionary[key]
-    else:
-        pass
-    return a_dictionary
+    # create a copy
+    find_to_dele = dict(list(a_dictionary.items())[:])
+    # check if value to delete is not none and type is a dict
+    if find_to_dele is not None and type(find_to_dele) is dict:
+        # iterate the tuple given by item(key, val)
+        for (key, val) in find_to_dele.items():
+            # check if it is the value to delete
+            if value == val:
+                # if it is, create a new dict
+                dele_dict = {key: val}
+            # iterate and delete the keys
+            for (key, val) in dele_dict.items():
+                del find_to_dele[key]
+        # return a copy when we are done
+        return find_to_dele
