c4486bb76611f85432ef5c7b26caa6e6df061458
Julius Kamara <juliuskamara79@gmail.com>
2023-04-01 10:38:34 -0700

    This project shows us to convert the whole database to utf8 character set

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,3 @@
 -- This project teaches us how to convert the entire table to utf8 character set
 
-USE hbtn_0c_0 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0 ALTER TABLE first_table MODIFY name TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
