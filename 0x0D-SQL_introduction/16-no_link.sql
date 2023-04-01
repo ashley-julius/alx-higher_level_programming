c3c4471d559ede1eb4a95b29939408ccddd1b8be
Julius Kamara <juliuskamara79@gmail.com>
2023-04-01 09:31:32 -0700

    This project teaches us how to list how many times a row occurs using… … an alias, and also how to group the results in a very nice format

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- This project shows us how to list two columns from a table, listed by order
-SELECT score, name FROM second_table ORDER score DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
