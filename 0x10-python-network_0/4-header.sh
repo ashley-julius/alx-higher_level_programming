7eb567f86103e6c9eca6a1c69b26b1ed8c615916
julius-ashley <juliuskamara79@gmail.com>
2023-06-23 01:26:39 -0700

    made some modifications

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script to send custom headers to servers
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+#pass a variable in the header of the request
+curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script to post data (url-encoded) to a server
-curl -s -d "email=hr@holbertonschool.com&subject=I%20will%20always%20be%20here%20for%20PLD" -X POST "$1"
+#Bash script that takes in a URL, sends a POST request to the passed URL
+curl -s -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
