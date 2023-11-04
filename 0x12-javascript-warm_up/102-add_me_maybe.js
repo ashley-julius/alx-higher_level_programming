99634778306c57bf306f36f8ff32f5fc7c333c98
Julius Kamara <110870660+ashley-julius@users.noreply.github.com>
2023-11-03 17:43:28 -0700

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-function addMeMaybe (number, theFunction) {
-  number += 1;
-  theFunction(number);
-}
-module.exports.addMeMaybe = addMeMaybe;
--- 0x12-javascript-warm_up/4-concat.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/node
-// copy the process.argv array
-const args = process.argv.slice(2);
-console.log(args[0] + ' is ' + args[1]);
